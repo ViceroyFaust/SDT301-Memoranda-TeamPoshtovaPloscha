2437070cfa8e6b68e1cf26a87e23988915aae3e0
Feshior <vladislavshvetsua@gmail.com>
2024-11-05 14:34:59 +0100

    Part of US1. Created base models
    Create base model needed for the bus schedule
    Prior to this change, there were no base models for the bus schedule.
    This commit introduces these changes
    Closes Task#4
    https://tree.taiga.io/project/feshior-1/task/4

---
+++ src/main/java/memoranda/busSchedule/models/Driver.java
@@ -0,0 +1,31 @@
+package memoranda.busSchedule.models;
+
+public class Driver {
+    private int id;
+    private String name;
+    private String phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/memoranda/busSchedule/models/Tour.java
@@ -0,0 +1,40 @@
+package memoranda.busSchedule.models;
+
+public class Tour {
+    private int id;
+    private String name;
+    private Bus bus;
+    private Route route;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public void setBus(Bus bus) {
+        this.bus = bus;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+}
