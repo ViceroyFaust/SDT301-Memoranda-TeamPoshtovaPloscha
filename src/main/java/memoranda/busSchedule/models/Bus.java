fb475de5e9b6e01406ee45802e26af5a0c252c9b
Vladyslav <vladislavshvetsua@gmail.com>
2024-11-23 22:30:28 +0100

    feat: Adjusted models for ApplicationContext

--- src/main/java/memoranda/busSchedule/models/Bus.java
+++ src/main/java/memoranda/busSchedule/models/Bus.java
@@ -1,14 +1,24 @@
 package memoranda.busSchedule.models;
 
-public class Bus {
+import memoranda.busSchedule.annotations.ForeignKey;
+import memoranda.busSchedule.annotations.PrimaryKey;
+import nu.xom.Element;
+
+public class Bus implements XMLable,IModel {
+    @PrimaryKey
     private int id;
     private int seats;
     private Driver driver;
+    //This is our connection to the driver
+    @ForeignKey(lazyLoadField = "driver", referencedClass = Driver.class)
+    private int driverId;
 
-    public Bus(int id, int seats, Driver driver) {
-        this.id = id;
+    public Bus(){
+
+    }
+    public Bus(int seats, Driver driver) {
         this.seats = seats;
-        this.driver = driver;
+        this.setDriver(driver);
     }
 
     public int getId() {
@@ -33,10 +43,38 @@ public class Bus {
 
     public void setDriver(Driver driver) {
         this.driver = driver;
+        this.driverId = driver.getId();
+    }
+    public int getDriverId(){
+        return driverId;
     }
 
     @Override
     public String toString() {
-        return "Bus(id: " + id + ", seats: " + seats + ", Driver: " + driver.toString() + ")";
+        if(driver !=null)
+            return "Bus(id: " + id + ", seats: " + seats + ", DriverID: " + driverId + ", Driver: " + driver.toString() + ")";
+        else
+            return "Bus(id: " + id + ", seats: " + seats + ", DriverID: " + driverId + ")";
+    }
+
+    @Override
+    public Element toXML() {
+        Element busElement = new Element("Bus");
+
+        Element id = new Element("id");
+        id.appendChild(String.valueOf(this.getId()));
+        busElement.appendChild(id);
+
+        Element seats = new Element("seats");
+        seats.appendChild(String.valueOf(this.getSeats()));
+        busElement.appendChild(seats);
+
+        return busElement;
+    }
+
+    @Override
+    public void fromXML(Element xlmElement) {
+        id = Integer.parseInt(xlmElement.getFirstChildElement("id").getValue());
+        seats = Integer.parseInt(xlmElement.getFirstChildElement("seats").getValue());
     }
 }
--- src/main/java/memoranda/busSchedule/models/Route.java
+++ src/main/java/memoranda/busSchedule/models/Route.java
@@ -1,14 +1,26 @@
 package memoranda.busSchedule.models;
 
+import memoranda.busSchedule.annotations.ForeignKey;
+import memoranda.busSchedule.annotations.PrimaryKey;
+import nu.xom.Element;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Route {
+public class Route implements XMLable, IModel {
+    @PrimaryKey
     private int id;
-    private Node[] nodes;
+    private LinkedList<Node> nodes = new LinkedList<>();
+    @ForeignKey(lazyLoadField = "nodes", referencedClass = Node.class)
+    List<Integer> nodesIds = new ArrayList<>();
 
-    public Route(int id, Node[] nodes) {
-        this.id = id;
-        this.nodes = nodes;
+    public Route() {
+
+    }
+    public Route(LinkedList<Node> nodes) {
+        this.setNodes(nodes);
     }
 
     public int getId() {
@@ -29,16 +41,43 @@ public class Route {
         return 0;
     }
 
-    public Node[] getNodes() {
+    public LinkedList<Node> getNodes() {
         return nodes;
     }
 
-    public void setNodes(Node[] nodes) {
-        this.nodes = nodes;
+    public void addNode(Node node) {
+        nodesIds.add(node.getId());
+        nodes.add(node);
+    }
+
+    public void setNodes(List<Node> nodes) {
+        for (Node node : nodes) {
+            nodesIds.add(node.getId());
+        }
+
+        this.nodes = new LinkedList<>(nodes);
     }
 
     @Override
     public String toString() {
-        return "Route(id: " + id + ", nodes: " + Arrays.toString(nodes) + ")";
+        return "Route(id: " + id + ", nodes size: " + nodes.size() + ")";
+    }
+
+    @Override
+    public Element toXML() {
+        Element routeElement = new Element("Route");
+
+        Element id = new Element("id");
+        id.appendChild(String.valueOf(this.getId()));
+        routeElement.appendChild(id);
+
+        //routeElement.appendChild(nodes);
+
+        return routeElement;
+    }
+
+    @Override
+    public void fromXML(Element xlmElement) {
+        id = Integer.parseInt(xlmElement.getFirstChildElement("id").getValue());
     }
 }
