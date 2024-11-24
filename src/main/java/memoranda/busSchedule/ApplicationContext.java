931a10a6e7f1908091f7e9a56f631531581537a9
Vladyslav <vladislavshvetsua@gmail.com>
2024-11-24 20:50:43 +0100

    refactor: ApplicationContext is made a Singletone
    Additionaly made the subsets final

--- src/main/java/memoranda/busSchedule/ApplicationContext.java
+++ src/main/java/memoranda/busSchedule/ApplicationContext.java
@@ -5,11 +5,24 @@ import memoranda.busSchedule.managers.ModelsSubset;
 import memoranda.busSchedule.models.*;
 
 public class ApplicationContext extends ModelsContext {
-    //region ModelsSubsets
-    public ModelsSubset<Bus> buses = new ModelsSubset<>(Bus.class);
-    public ModelsSubset<Driver> drivers = new ModelsSubset<>(Driver.class);
-    public ModelsSubset<Node> nodes = new ModelsSubset<>(Node.class);
-    public ModelsSubset<Route> routes = new ModelsSubset<>(Route.class);
-    public ModelsSubset<Tour> tours = new ModelsSubset<>(Tour.class);
+    //region Singleton
+    private static final ApplicationContext instance = new ApplicationContext();
+
+    public static ApplicationContext getInstance() {
+        return instance;
+    }
+
+    private ApplicationContext() {
+    }
+
     //endregion
+
+    //region ModelsSubsets
+    public final ModelsSubset<Bus> buses = new ModelsSubset<>(Bus.class);
+    public final ModelsSubset<Driver> drivers = new ModelsSubset<>(Driver.class);
+    public final ModelsSubset<Node> nodes = new ModelsSubset<>(Node.class);
+    public final ModelsSubset<Route> routes = new ModelsSubset<>(Route.class);
+    public final ModelsSubset<Tour> tours = new ModelsSubset<>(Tour.class);
+    //endregion
+
 }
